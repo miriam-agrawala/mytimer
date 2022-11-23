69a247336323e374b256d47f10191829a7e61374
mriam-agrawala <m.agrawala@gmx.de>
2022-11-23 11:51:45 +0100

    dummy timer

---
+++ run.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 11:47:51 2022
+
+@author: magra
+"""
+
+import datetime
+
+current_time = datetime.datetime.now()
+
+print(f"Time now at greenwich is: {current_time}")
